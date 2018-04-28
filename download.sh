c080feb1de88a0efb17974415f5fe3a0e09973e9
Jim Booth <jb80016@users.noreply.github.com>
2018-04-27 20:41:37 -0600

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
 export YEAR=${YEAR:=2015}
+
 echo "Downloading YEAR=$YEAR..."
+
 for MONTH in `seq -w 1 12`; do
-echo $YEAR$MONTH
-PARAMS="UserTableName=On_Time_Performance&DBShortName=&RawDataTable=T_ONTIME&sqlstr=+SELECT+FL_DATE%2CUNIQUE_CARRIER%2CAIRLINE_ID%2CCARRIER%2CF$
-RESPONSE=$(curl -X POST --data "$PARAMS" https://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0)
-echo "Received $RESPONSE"
-ZIPFILE=$(echo $RESPONSE | tr '\"' '\n' | grep zip)
-echo $ZIPFILE
-curl -o $YEAR$MONTH.zip $ZIPFILE
+
+  echo $YEAR$MONTH
+  PARAMS="UserTableName=On_Time_Performance&DBShortName=&RawDataTable=T_ONTIME&sqlstr=+SELECT+FL_DATE%2CUNIQUE_CARRIER%2CAIRLINE_ID%2CCARRIER%2CF$
+  RESPONSE=$(curl -X POST --data "$PARAMS" https://www.transtats.bts.gov/DownLoad_Table.asp?Table_ID=236&Has_Group=3&Is_Zipped=0)
+  
+  echo "Received $RESPONSE"
+  ZIPFILE=$(echo $RESPONSE | tr '\"' '\n' | grep zip)
+
+  echo $ZIPFILE
+  curl -o $YEAR$MONTH.zip $ZIPFILE
+
 done
